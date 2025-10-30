b50072ab12c4cc94b56cbd7fa1ed3796da0e442a
Mishal Zaman <mishalzaman@gmail.com>
2025-10-30 08:50:39 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -111,17 +111,8 @@ int main() {
 
     enum
     {
-        // I use this ISceneNode ID to indicate a scene node that is
-        // not pickable by getSceneNodeAndCollisionPointFromRay()
         ID_IsNotPickable = 0,
-
-        // I use this flag in ISceneNode IDs to indicate that the
-        // scene node can be picked by ray selection.
         IDFlag_IsPickable = 1 << 0,
-
-        // I use this flag in ISceneNode IDs to indicate that the
-        // scene node can be highlighted.  In this example, the
-        // homonids can be highlighted, but the level mesh can't.
         IDFlag_IsHighlightable = 1 << 1
     };
 
@@ -275,7 +266,7 @@ int main() {
                         // Calculate the path and store it in the member variable 'currentPath'
                         currentPath = pathfinding->getPath(startPos, intersectionPoint);
                     }
-                }
+                } 
                 else {
                     // The ray didn't hit any node *flagged as pickable*
                     std::cout << "Mouse ray missed all pickable nodes." << std::endl;
